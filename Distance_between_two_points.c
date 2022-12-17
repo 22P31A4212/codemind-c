03d71fe4d46cb4fa4332856829ccc1779b80f55b
22P31A4212 <119730521+22P31A4212@users.noreply.github.com>
2022-12-17 21:49:53 +0530

    #include <stdio.h> #include <math.h> int main() { 	float x1, y1, x2, y2, gdistance; 	scanf("%f", &x1); 	scanf("%f", &y1); 	scanf("%f", &x2); 	scanf("%f", &y2); 	gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)); 	printf("%.4f",sqrt(gdistance)); 	printf(" "); 	return 0; }

---
+++ Distance_between_two_points.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float x1, y1, x2, y2, gdistance;
+	scanf("%f", &x1);
+	scanf("%f", &y1);
+	scanf("%f", &x2);
+	scanf("%f", &y2);
+	gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+	printf("%.4f",sqrt(gdistance));
+	printf("
+");
+	return 0;
+}
