4772febac9f54357eaf080d55ebe3f16388a592f
Pranav <119730521+22P31A4212@users.noreply.github.com>
2022-12-18 01:20:07 +0530

    #include <stdio.h> int main() {     int n1, n2;     scanf("%d %d",&n1,&n2);     while(n1!=n2)     {         if(n1 > n2)         n1 -= n2;         else         n2 -= n1;     }     printf("%d",n1); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2;
+    scanf("%d %d",&n1,&n2);
+    while(n1!=n2)
+    {
+        if(n1 > n2)
+        n1 -= n2;
+        else
+        n2 -= n1;
+    }
+    printf("%d",n1);
+}
