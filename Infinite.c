288054320eb480cd42fa6c55db816da39235c13a
Pranav <119730521+22P31A4212@users.noreply.github.com>
2022-12-22 22:17:02 +0530

    #include<stdio.h> int main() {     for(int i=1;;i++)     {         int n;         scanf("%d",&n);         if(n==-1)         {             break;         }         else         {             printf("%d ",n*n);         }     } }

---
+++ Infinite.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n==-1)
+        {
+            break;
+        }
+        else
+        {
+            printf("%d
+",n*n);
+        }
+    }
+}
