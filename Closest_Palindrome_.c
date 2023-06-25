fae1d0f68aba9c2bede5e76107bdceb58a727027
Pranav <119730521+22P31A4212@users.noreply.github.com>
2023-06-25 07:50:55 +0530

    #include<stdio.h> int main() {     int n,i,j,r,cp,sp,d,e;     scanf("%d",&n);//121         for(i=n-1;i>=n-20;i--)//i=111>=111         {             j=i;//j=111             r=0;//r=0             while(j>0)//1>0             {                 r=r*10+j%10;//r=111                 j/=10;//j=0             }             if(r==i)//21==120             {                 cp=i;//cp=111                 break;             }         }         for(i=n+1;i<=n+20;i++)//i=131<=131         {         	j=i;//j=131             r=0;             while(j>0)//131>0             {                 r=r*10+j%10;//r=131                 j/=10;//j=0             }             if(r==i)//r==131             {                 sp=i;//sp=131                 break;             }         }         d=n-cp;//d=121-111=10         e=sp-n;//e=131-121=10         if(d<e)//         {             printf("%d",cp);         }         else if(d==e)         {             printf("%d %d",cp,sp);         }         else         {             printf("%d",sp);         } }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,56 +1,50 @@
 #include<stdio.h>
-int pal(int n)
-{
-    int t=n;
-    int r,rv=0;
-    while(n>0)
-    {
-        r=n%10;
-        rv=rv*10+r;
-        n=n/10;
-    }
-    if(rv==t)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-    
-}
 int main()
 {
-    int n,f,b;
-    scanf("%d",&n);
-    for(int i=n+1;;i++)
-    {
-        if(pal(i))
+    int n,i,j,r,cp,sp,d,e;
+    scanf("%d",&n);//121
+        for(i=n-1;i>=n-20;i--)//i=111>=111
         {
-            f=i;
-            break;
+            j=i;//j=111
+            r=0;//r=0
+            while(j>0)//1>0
+            {
+                r=r*10+j%10;//r=111
+                j/=10;//j=0
+            }
+            if(r==i)//21==120
+            {
+                cp=i;//cp=111
+                break;
+            }
         }
-    }
-    for(int i=n-1;;i--)
-    {
-        if(pal(i))
+        for(i=n+1;i<=n+20;i++)//i=131<=131
         {
-            b=i;
-            break;
+        	j=i;//j=131
+            r=0;
+            while(j>0)//131>0
+            {
+                r=r*10+j%10;//r=131
+                j/=10;//j=0
+            }
+            if(r==i)//r==131
+            {
+                sp=i;//sp=131
+                break;
+            }
         }
-    }
-    int d1=f-n;
-    int d2=n-b;
-    if(d1<d2)
-    {
-        printf("%d",f);
-    }
-    else if(d2<d1)
-    {
-        printf("%d",b);
-    }
-    else if(d1==d2)
-    {
-        printf("%d %d",b,f);
-    }
+        d=n-cp;//d=121-111=10
+        e=sp-n;//e=131-121=10
+        if(d<e)//
+        {
+            printf("%d",cp);
+        }
+        else if(d==e)
+        {
+            printf("%d %d",cp,sp);
+        }
+        else
+        {
+            printf("%d",sp);
+        }
 }
