04c5dd71a7c50e4b68a321ecd541ed48cbc6c2a4
Pranav <119730521+22P31A4212@users.noreply.github.com>
2023-06-27 20:59:51 +0530

    #include<stdio.h> int main() {     int i,n,n1,n2,n3,j,np,sp,d,e;     scanf("%d",&n);     for(i=n;i>=n-100;i--)     {     	n1=0;         n2=1;     	for(j=1;j<=i-2;j++)     	{             n3=n1+n2;             if(n3==i)             {                 np=n3;                 d=n-np;                 break;             }             else if(n3>i)             {             	break; 			}             n1=n2;             n2=n3;         }         if(n3==i)         {             break;         }     }     for(i=n;i<=n+100;i++)     {     	n1=0;         n2=1;     	for(j=1;j<=i-2;j++)     	{             n3=n1+n2;             if(n3==i)             {                 sp=n3;                 e=sp-n;                 break;             }             else if(n3>i)             {             	break; 			}             n1=n2;             n2=n3;         }         if(n3==i)         {             break;         }     }     if(d<e)     {     	printf("%d",np); 	} 	else if(d>e) 	{ 		printf("%d",sp); 	} 	else 	{ 		printf("%d %d",np,sp); 	} }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,24 +1,68 @@
 #include<stdio.h>
 int main()
 {
-    int first=0,second=1,next=0,number;
-    scanf("%d",&number);
-    while(number>next)
+    int i,n,n1,n2,n3,j,np,sp,d,e;
+    scanf("%d",&n);
+    for(i=n;i>=n-100;i--)
     {
-        next=first+second;
-        first=second;
-        second=next;
+    	n1=0;
+        n2=1;
+    	for(j=1;j<=i-2;j++)
+    	{
+            n3=n1+n2;
+            if(n3==i)
+            {
+                np=n3;
+                d=n-np;
+                break;
+            }
+            else if(n3>i)
+            {
+            	break;
+			}
+            n1=n2;
+            n2=n3;
+        }
+        if(n3==i)
+        {
+            break;
+        }
     }
-    if(number-first<second-number)
+    for(i=n;i<=n+100;i++)
     {
-        printf("%d",first);
+    	n1=0;
+        n2=1;
+    	for(j=1;j<=i-2;j++)
+    	{
+            n3=n1+n2;
+            if(n3==i)
+            {
+                sp=n3;
+                e=sp-n;
+                break;
+            }
+            else if(n3>i)
+            {
+            	break;
+			}
+            n1=n2;
+            n2=n3;
+        }
+        if(n3==i)
+        {
+            break;
+        }
     }
-    else if(number-first==second-number)
+    if(d<e)
     {
-        printf("%d %d",first,second);
-    }
-    else
-    {
-        printf("%d",second);
-    }
+    	printf("%d",np);
+	}
+	else if(d>e)
+	{
+		printf("%d",sp);
+	}
+	else
+	{
+		printf("%d %d",np,sp);
+	}
 }
