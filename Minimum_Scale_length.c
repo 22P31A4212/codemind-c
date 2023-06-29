368ef4a7f59791df4dda5ea91fd0cbabfb6eae9f
Pranav <119730521+22P31A4212@users.noreply.github.com>
2023-06-29 21:58:20 +0530

    #include<stdio.h> int main() {     int n,i,j,f=0,min=999;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(min>a[i])         {             min=a[i];         }     }     for(i=min;i>=1;i--)     {         f=0;         for(j=0;j<n;j++)         {             if(a[j]%i!=0)             {                 f=1;                 break;             }         }         if(f==0)         {             printf("%d",i);             break;         }     } }

--- Minimum_Scale_length.c
+++ Minimum_Scale_length.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 int main()
 {
-    int n,scales[10],i,j,found=0,min=9999;
+    int n,i,j,f=0,min=999;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    scanf("%d",&scales[i]);
+    int a[n];
     for(i=0;i<n;i++)
     {
-        if(min>scales[i])
-            min=scales[i];
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+        }
     }
     for(i=min;i>=1;i--)
     {
-        found=0;
+        f=0;
         for(j=0;j<n;j++)
         {
-            if(scales[j]%i!=0)
-                {
-                    found=1;
-                    break;
-                }
+            if(a[j]%i!=0)
+            {
+                f=1;
+                break;
+            }
         }
-        if(found==0)
+        if(f==0)
         {
             printf("%d",i);
             break;
         }
     }
-    return 0;
 }
