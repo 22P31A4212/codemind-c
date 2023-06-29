50e2510563d5faaf22a9d5ac132ea7d5e89813cf
Pranav <119730521+22P31A4212@users.noreply.github.com>
2023-06-29 21:34:07 +0530

    #include<stdio.h> int main() {     int i,j,n,t;     scanf("%d",&n);     for (i=1,t=(64+i);i<=n;i++,t++)     {         for(j=1;j<=n;j++)         {             printf("%c ",t);         }         printf(" ");     } }

---
+++ print_the_following_pattern_11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,t;
+    scanf("%d",&n);
+    for (i=1,t=(64+i);i<=n;i++,t++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%c ",t);
+        }
+        printf("
+");
+    }
+}
